9667789d9a07f50fa6a3eb3fb829ff0ac1db497e
Zouroboros <zouroboros@zoho.eu>
2023-05-05 23:34:30 +0200

    Added example how to iterate over an array in javascript.

---
+++ patterns/javascript/iterate-over-an-array/example.js
@@ -0,0 +1,11 @@
+const fruits = ["Apple", "Banana", "Cocos", "Dates", "Eggplant"];
+
+// for of loop
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+
+// loop using forEach
+fruits.forEach(fruit => {
+    console.log(fruit);
+});
---
+++ patterns/javascript/iterate-over-an-array/info.md
@@ -0,0 +1,5 @@
+# Iterate over an array
+
+In javascript there are multiple ways of iterating over a list. The first
+possibility is to use a for of loop. The second is to use the forEach 
+function of arrays.
