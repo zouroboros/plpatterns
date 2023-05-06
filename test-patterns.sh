d849a1e019ff535ff58f68ba95b357f4574a35f9
Zouroboros <zouroboros@zoho.eu>
2023-05-06 11:22:09 +0200

    Added test for C files.

--- test-patterns.sh
+++ test-patterns.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell 
 #! nix-shell -i bash --pure
-#! nix-shell -p nodejs-slim python3
+#! nix-shell -p nodejs-slim python3 gcc
 #! nix-shell -I nixpkgs=channel:nixos-22.11
 
 for file in patterns/javascript/*/*.js; 
@@ -12,3 +12,10 @@ for file in patterns/python/*/*.py;
 do 
     python3 $file; 
 done
+
+mkdir .build
+for file in patterns/C/*/*.c; 
+do 
+    cc $file -o .build/app.out; 
+done
+rm -rf .build
